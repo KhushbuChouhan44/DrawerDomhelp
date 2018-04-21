57d8587162bee5fbbb35882955e0699ce7ac8af5
Khushbu <khushbuchouhan44@gmail.com>
2018-04-21 20:18:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/dashboarddomhelp/WorkerDashboard.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.dashboarddomhelp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkerDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_worker);
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/dashboarddomhelp/dash_worker.java
@@ -0,0 +1,52 @@
+package com.example.lenovo.dashboarddomhelp;
+
+import android.app.ActionBar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toolbar;
+
+public class dash_worker extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_worker);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    private void setSupportActionBar(Toolbar toolbar) {
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
+ 
